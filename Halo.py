8529ce8df786a6577317a514fd72932fd1cbd14e
Febrinaads <82952360+Febrinaads@users.noreply.github.com>
2021-04-22 10:03:17 +0700

    Add files via upload

---
+++ Halo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print ("#/=========================================/")
+print ("/                                          /")
+print ("/            program hello.py              /")
+print ("/   by febrinadyahsukmawati74@mail.ugm.ac.id    /")
+print ("/                                          /")
+print ("#/=========================================/")
+
+
+# In[ ]:
+
+
+
+
